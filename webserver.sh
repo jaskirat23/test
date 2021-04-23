63f8a9da32f3cdbbfa3ac16cdb37d691dc224b51
Jaskirat <singh.jaskirat23@gmail.com>
2021-04-23 10:04:51 +0000

    updated script

--- webserver.sh
+++ webserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum install httpd 
+yum install -y httpd 
 echo "<h1>This webpage was built with the help of Jenkins server</h1>" > /var/www/html/index.html
 systemctl start httpd
-systemctl status https
+systemctl status httpd
 sleep 10
